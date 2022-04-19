c9e4f7d9f1d42d1114fcfeae3bd4346b757b4bfc
Hawk <wildwolf916@hotmail.com>
2022-04-20 01:22:05 +0800

    2022.04.20 update

--- FtxApi_Test/Program.cs
+++ FtxApi_Test/Program.cs
@@ -94,58 +94,33 @@ namespace FtxApi_Test
         #region BuyAndSell
         private static async Task BuyAndSell(FtxRestApi api)
         {
-            var ins = "APE/USD";
+            var ins = "STG/USD";
+            var ins1 = "APE/USD";
 
-            var dateStart = DateTime.UtcNow.AddMinutes(-100);
-            var dateEnd = DateTime.UtcNow.AddMinutes(-10);
-
-            var r1 = api.GetCoinsAsync().Result;
-            //Console.WriteLine(r1);
-            var r2 = api.GetAllFuturesAsync().Result;
-            //Console.WriteLine(r2);
-            var r3 = api.GetFutureAsync(ins).Result;
-            //Console.WriteLine(r3);
-            var r4 = api.GetFutureStatsAsync(ins).Result;
-            //Console.WriteLine(r4);
-            var r5 = api.GetFundingRatesAsync(dateStart, dateEnd).Result;
-            var r6 = api.GetHistoricalPricesAsync(ins, 300, 30, dateStart, dateEnd).Result;
-            var r7 = api.GetMarketsAsync().Result;
             var r8 = api.GetSingleMarketsAsync(ins).Result;
-            //Console.WriteLine(r8);
-            var r9 = api.GetMarketOrderBookAsync(ins, 20).Result;
-            var r10 = api.GetMarketTradesAsync(ins, 20, dateStart, dateEnd).Result;
-            var r11 = api.GetAccountInfoAsync().Result;
-            var r12 = api.GetPositionsAsync().Result;
-            var r13 = api.ChangeAccountLeverageAsync(20).Result;
-
+            Console.WriteLine(r8);
+ 
             var r14 = api.GetCoinAsync().Result;
-            //Console.WriteLine(r14);
+            Console.WriteLine(r14);
             var r15 = api.GetBalancesAsync().Result;
-            var BalanceList = JsonConvert.DeserializeObject<Result>(r15);
-            Console.WriteLine(BalanceList);
-            var r16 = api.GetDepositAddressAsync("BTC").Result;
-            var r17 = api.GetDepositHistoryAsync().Result;
-            var r18 = api.GetWithdrawalHistoryAsync().Result;
-            var r19 = api.RequestWithdrawalAsync("USDTBEAR", 20.2m, "0x83a127952d266A6eA306c40Ac62A4a70668FE3BE", "", "", "").Result;
-            var r21 = api.GetOpenOrdersAsync(ins).Result;
+            Result BalanceResult = JsonConvert.DeserializeObject<Result>(r15);
+            var BalanceList = BalanceResult.result;
+            foreach (var item in BalanceList)
+            {
+                if (item.coin == "STG")
+                {
+                    Console.WriteLine("Coin: " + item.coin);
+                    Console.WriteLine("UsdValue: " + item.usdValue);
+                    Console.WriteLine("Total: " + item.total);
+                    Console.WriteLine("Free: " + item.free);
+                    Console.WriteLine("AvailableWithoutBorrow: " + item.availableWithoutBorrow);
+                    Console.WriteLine("SpotBorrow: " + item.spotBorrow);
+                }
+            } 
+
             var r20 = api.PlaceOrderAsync(ins, SideType.buy, 1000, OrderType.limit, 0.001m, false).Result;
             var r20_1 = api.PlaceStopOrderAsync(ins, SideType.buy, 1000, 0.001m, false).Result;
-            var r20_2 = api.PlaceTrailingStopOrderAsync(ins, SideType.buy, 0.05m, 0.001m, false).Result;
-            var r20_3 = api.PlaceTakeProfitOrderAsync(ins, SideType.buy, 1000, 0.001m, false).Result;
-            var r23 = api.GetOrderStatusAsync("12345").Result;
-            var r24 = api.GetOrderStatusByClientIdAsync("12345").Result;
-            var r25 = api.CancelOrderAsync("1234").Result;
-            var r26 = api.CancelOrderByClientIdAsync("12345").Result;
-            var r27 = api.CancelAllOrdersAsync(ins).Result;
-            var r28 = api.GetFillsAsync(ins, 20, dateStart, dateEnd).Result;
-            var r29 = api.GetFundingPaymentAsync(dateStart, dateEnd).Result;
-            var r30 = api.GetLeveragedTokensListAsync().Result;
-            var r31 = api.GetTokenInfoAsync("HEDGE").Result;
-            var r32 = api.GetLeveragedTokenBalancesAsync().Result;
-            var r33 = api.GetLeveragedTokenCreationListAsync().Result;
-            var r34 = api.RequestLeveragedTokenCreationAsync("HEDGE", 100).Result;
-            var r35 = api.GetLeveragedTokenRedemptionListAsync().Result;
-            var r36 = api.RequestLeveragedTokenRedemptionAsync("HEDGE", 100).Result;
+           
         }
         #endregion
 
