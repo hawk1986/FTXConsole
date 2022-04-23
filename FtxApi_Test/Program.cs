d85f564c0aa45d1001827c41db8835f334a39310
Hawk <wildwolf916@hotmail.com>
2022-04-23 23:19:26 +0800

    2022.04.23 update

--- FtxApi_Test/Program.cs
+++ FtxApi_Test/Program.cs
@@ -31,82 +31,10 @@ namespace FtxApi_Test
             //RestTests(api).Wait();
 
             //WebSocketTests(wsApi, client).Wait();
+
             Console.ReadLine();
 
-
-
         }
-
-        #region RestTests
-        private static async Task RestTests(FtxRestApi api)
-        {
-            var ins = "APE/USD";
-
-            var dateStart = DateTime.UtcNow.AddMinutes(-100);
-            var dateEnd = DateTime.UtcNow.AddMinutes(-10);
-
-            var r1 = api.GetCoinsAsync().Result;
-            var r2 = api.GetAllFuturesAsync().Result;
-            var r3 = api.GetFutureAsync(ins).Result;
-            var r4 = api.GetFutureStatsAsync(ins).Result;
-            var r5 = api.GetFundingRatesAsync(dateStart, dateEnd).Result;
-            var r6 = api.GetHistoricalPricesAsync(ins, 300, 30, dateStart, dateEnd).Result;
-            var r7 = api.GetMarketsAsync().Result;
-            var r8 = api.GetSingleMarketsAsync(ins).Result;
-            var r9 = api.GetMarketOrderBookAsync(ins, 20).Result;
-            var r10 = api.GetMarketTradesAsync(ins, 20, dateStart, dateEnd).Result;
-            var r11 = api.GetAccountInfoAsync().Result;
-            var r12 = api.GetPositionsAsync().Result;
-            var r13 = api.ChangeAccountLeverageAsync(20).Result;
-            var r14 = api.GetCoinAsync().Result;
-            var r15 = api.GetBalancesAsync().Result;
-            var r16 = api.GetDepositAddressAsync("BTC").Result;
-            var r17 = api.GetDepositHistoryAsync().Result;
-            var r18 = api.GetWithdrawalHistoryAsync().Result;
-            var r19 = api.RequestWithdrawalAsync("USDTBEAR", 20.2m, "0x83a127952d266A6eA306c40Ac62A4a70668FE3BE", "", "", "").Result;
-            var r21 = api.GetOpenOrdersAsync(ins).Result;
-            Console.WriteLine(r21);
-            var r20 = api.PlaceOrderAsync(ins, SideType.buy, 1000, OrderType.limit, 0.001, false).Result;
-            var r20_1 = api.PlaceStopOrderAsync(ins, SideType.buy, 1000, 0.001m, false).Result;
-            var r20_2 = api.PlaceTrailingStopOrderAsync(ins, SideType.buy, 0.05m, 0.001m, false).Result;
-            var r20_3 = api.PlaceTakeProfitOrderAsync(ins, SideType.buy, 1000, 0.001m, false).Result;
-            var r23 = api.GetOrderStatusAsync("12345").Result;
-            var r24 = api.GetOrderStatusByClientIdAsync("12345").Result;
-            var r25 = api.CancelOrderAsync("1234").Result;
-            var r26 = api.CancelOrderByClientIdAsync("12345").Result;
-            var r27 = api.CancelAllOrdersAsync(ins).Result;
-            var r28 = api.GetFillsAsync(ins, 20, dateStart, dateEnd).Result;
-            var r29 = api.GetFundingPaymentAsync(dateStart, dateEnd).Result;
-            var r30 = api.GetLeveragedTokensListAsync().Result;
-            var r31 = api.GetTokenInfoAsync("HEDGE").Result;
-            var r32 = api.GetLeveragedTokenBalancesAsync().Result;
-            var r33 = api.GetLeveragedTokenCreationListAsync().Result;
-            var r34 = api.RequestLeveragedTokenCreationAsync("HEDGE", 100).Result;
-            var r35 = api.GetLeveragedTokenRedemptionListAsync().Result;
-            var r36 = api.RequestLeveragedTokenRedemptionAsync("HEDGE", 100).Result;
-        }
-        #endregion
-
-        #region WebSocketTests
-        private static async Task WebSocketTests(FtxWebSocketApi wsApi, Client client)
-        {
-            var ins = "APE/USD";
-
-            wsApi.OnWebSocketConnect += () =>
-            {
-                //wsApi.SendCommand(FtxWebSockerRequestGenerator.GetAuthRequest(client));
-                wsApi.SendCommand(FtxWebSockerRequestGenerator.GetSubscribeRequest("orderbook", ins));
-                //wsApi.SendCommand(FtxWebSockerRequestGenerator.GetSubscribeRequest("trades", ins));
-                //wsApi.SendCommand(FtxWebSockerRequestGenerator.GetSubscribeRequest("ticker", ins));
-                //wsApi.SendCommand(FtxWebSockerRequestGenerator.GetSubscribeRequest("fills"));
-                //wsApi.SendCommand(FtxWebSockerRequestGenerator.GetSubscribeRequest("orders"));
-
-
-            };
-
-            await wsApi.Connect();
-        }
-        #endregion
 
         #region BuyAndSell
         private static async Task BuyAndSell(FtxRestApi api)
@@ -147,6 +75,7 @@ namespace FtxApi_Test
                 if (item.market == "APE/USD" && item.side == "buy")
                 {
                     var cancel = api.CancelOrderAsync(item.id);
+                    Console.WriteLine("Order canceled!");
                 }
                 else if (item.market == "APE/USD" && item.side == "sell")
                 {
@@ -218,7 +147,7 @@ namespace FtxApi_Test
                     }
                     #endregion
 
-                    buyPrice = (askPrice - 0.1);
+                    buyPrice = (askPrice - 0.05);
 
                     var getApeBalance = api.GetBalancesAsync().Result;
                     BalanceResult ApeBalanceResult = JsonConvert.DeserializeObject<BalanceResult>(getApeBalance);
@@ -330,6 +259,77 @@ namespace FtxApi_Test
             var Market_Buy = MarketResult_Buy.result;
             askprice = Market_Buy.ask ?? 0;
             return askprice; 
+        }
+        #endregion
+
+        #region RestTests
+        private static async Task RestTests(FtxRestApi api)
+        {
+            var ins = "APE/USD";
+
+            var dateStart = DateTime.UtcNow.AddMinutes(-100);
+            var dateEnd = DateTime.UtcNow.AddMinutes(-10);
+
+            var r1 = api.GetCoinsAsync().Result;
+            var r2 = api.GetAllFuturesAsync().Result;
+            var r3 = api.GetFutureAsync(ins).Result;
+            var r4 = api.GetFutureStatsAsync(ins).Result;
+            var r5 = api.GetFundingRatesAsync(dateStart, dateEnd).Result;
+            var r6 = api.GetHistoricalPricesAsync(ins, 300, 30, dateStart, dateEnd).Result;
+            var r7 = api.GetMarketsAsync().Result;
+            var r8 = api.GetSingleMarketsAsync(ins).Result;
+            var r9 = api.GetMarketOrderBookAsync(ins, 20).Result;
+            var r10 = api.GetMarketTradesAsync(ins, 20, dateStart, dateEnd).Result;
+            var r11 = api.GetAccountInfoAsync().Result;
+            var r12 = api.GetPositionsAsync().Result;
+            var r13 = api.ChangeAccountLeverageAsync(20).Result;
+            var r14 = api.GetCoinAsync().Result;
+            var r15 = api.GetBalancesAsync().Result;
+            var r16 = api.GetDepositAddressAsync("BTC").Result;
+            var r17 = api.GetDepositHistoryAsync().Result;
+            var r18 = api.GetWithdrawalHistoryAsync().Result;
+            var r19 = api.RequestWithdrawalAsync("USDTBEAR", 20.2m, "0x83a127952d266A6eA306c40Ac62A4a70668FE3BE", "", "", "").Result;
+            var r21 = api.GetOpenOrdersAsync(ins).Result;
+            Console.WriteLine(r21);
+            var r20 = api.PlaceOrderAsync(ins, SideType.buy, 1000, OrderType.limit, 0.001, false).Result;
+            var r20_1 = api.PlaceStopOrderAsync(ins, SideType.buy, 1000, 0.001m, false).Result;
+            var r20_2 = api.PlaceTrailingStopOrderAsync(ins, SideType.buy, 0.05m, 0.001m, false).Result;
+            var r20_3 = api.PlaceTakeProfitOrderAsync(ins, SideType.buy, 1000, 0.001m, false).Result;
+            var r23 = api.GetOrderStatusAsync("12345").Result;
+            var r24 = api.GetOrderStatusByClientIdAsync("12345").Result;
+            var r25 = api.CancelOrderAsync("1234").Result;
+            var r26 = api.CancelOrderByClientIdAsync("12345").Result;
+            var r27 = api.CancelAllOrdersAsync(ins).Result;
+            var r28 = api.GetFillsAsync(ins, 20, dateStart, dateEnd).Result;
+            var r29 = api.GetFundingPaymentAsync(dateStart, dateEnd).Result;
+            var r30 = api.GetLeveragedTokensListAsync().Result;
+            var r31 = api.GetTokenInfoAsync("HEDGE").Result;
+            var r32 = api.GetLeveragedTokenBalancesAsync().Result;
+            var r33 = api.GetLeveragedTokenCreationListAsync().Result;
+            var r34 = api.RequestLeveragedTokenCreationAsync("HEDGE", 100).Result;
+            var r35 = api.GetLeveragedTokenRedemptionListAsync().Result;
+            var r36 = api.RequestLeveragedTokenRedemptionAsync("HEDGE", 100).Result;
+        }
+        #endregion
+
+        #region WebSocketTests
+        private static async Task WebSocketTests(FtxWebSocketApi wsApi, Client client)
+        {
+            var ins = "APE/USD";
+
+            wsApi.OnWebSocketConnect += () =>
+            {
+                //wsApi.SendCommand(FtxWebSockerRequestGenerator.GetAuthRequest(client));
+                wsApi.SendCommand(FtxWebSockerRequestGenerator.GetSubscribeRequest("orderbook", ins));
+                //wsApi.SendCommand(FtxWebSockerRequestGenerator.GetSubscribeRequest("trades", ins));
+                //wsApi.SendCommand(FtxWebSockerRequestGenerator.GetSubscribeRequest("ticker", ins));
+                //wsApi.SendCommand(FtxWebSockerRequestGenerator.GetSubscribeRequest("fills"));
+                //wsApi.SendCommand(FtxWebSockerRequestGenerator.GetSubscribeRequest("orders"));
+
+
+            };
+
+            await wsApi.Connect();
         }
         #endregion
     }
